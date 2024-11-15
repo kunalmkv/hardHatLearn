5e0cd9c3e776ac65da17bcb54005706be7c433b4
kunalmkv <kunalm4.kvtagore@gmail.com>
2024-11-15 20:53:52 +0530

    proxy | stable | learn

---
+++ contracts/box.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract Box {
+    uint256 public value;
+
+    function setValue(uint256 _value) public {
+        value = _value;
+    }
+
+}
+
+/*
+Proxy contract address: 0x1DA4235bA09377415cb5105A7d4749b152cc63AF
+Implementation contract address: 0x5B2bd054c299Bc36C6266a6721D383E085237CA1
+Admin contract address: 0xd56c7C7b6902fE98a986e21c48eDe57Eac0C397B
+*/
